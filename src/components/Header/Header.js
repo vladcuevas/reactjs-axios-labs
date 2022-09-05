4c7c31cea1cd83e93bed4e1c52feb7915a5443d5
vladcuevas <vladcuevas@gmail.com>
2022-09-05 16:02:25 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,40 +1,42 @@
 import React from 'react'
 import './Header.css'
 import LocalPharmacyIcon from '@mui/icons-material/LocalPharmacy';
-import SearchIcon from '@mui/icons-material/Search';
 import { Link } from "react-router-dom"
+
+
 
 import AdminPortalLink from './AdminPortalLink';
 // react bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // end react bootstrap
 
 function Header() {
     return (
         <div className="header">
-            <Link to="/" style={{ textDecoration: "none" }}>
-                <div className="header__logo">
-                    <LocalPharmacyIcon className="header__logoImage" fontSize="large" />
-                    <h2 className="header__logoTitle">E-Health</h2>
-                </div>
-            </Link>
-
-            <div className="header__search">
-                <input type="text" className="header__searchInput" />
-                <SearchIcon className="header__searchIcon" />
-            </div>
-
-            <div className="header__nav">
-                <Link to="/" style={{ textDecoration: "none" }}>
-                    <div className="nav__item">
-                        <span className="nav__itemLineOne">Hello Guest</span>
-                        <span className="nav__itemLineTwo">Sign In</span>
-                    </div>
-                </Link>
-
-                <AdminPortalLink />
-
-            </div>
+            <Container >
+                <Row className="justify-content-md-center">
+                    <Col md="auto">
+                        <Link to="/" style={{ textDecoration: "none" }}>
+                            <div className="header__logo">
+                                <LocalPharmacyIcon className="header__logoImage" fontSize="large" />
+                                <h2 className="header__logoTitle">E-Health</h2>
+                            </div>
+                        </Link>
+                    </Col>
+                    <Col className="header__nav" md="auto">
+                        <Link to="/" style={{ textDecoration: "none" }}>
+                            <div className="nav__item">
+                                <span className="nav__itemLineOne">Hello Guest</span>
+                                <span className="nav__itemLineTwo">Sign In</span>
+                            </div>
+                        </Link>
+                        <AdminPortalLink />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
